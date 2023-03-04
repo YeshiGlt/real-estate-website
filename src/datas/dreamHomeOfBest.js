6d870cca1a733b50d554c1b332e5ac0e174d9c9e
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-03-04 15:40:52 +0000

    📦 NEW: Datalist des maisons/appartements Dream Home Of the Best

---
+++ src/datas/dreamHomeOfBest.js
@@ -0,0 +1,33 @@
+import heyjackson1 from "../assets/hey-jackson/hey-jackson-house-picture-1.png";
+import heyjackson2 from "../assets/hey-jackson/hey-jackson-house-picture-2.png";
+import heyjackson3 from "../assets/hey-jackson/hey-jackson-house-picture-3.png";
+import heyjackson4 from "../assets/hey-jackson/hey-jackson-house-picture-4.png";
+import heyjackson5 from "../assets/hey-jackson/hey-jackson-house-picture-5.png";
+
+export const dreamHomeOfBest = [
+  {
+    id: "h2u",
+    name: "heyJacksonHouse1",
+    picture: heyjackson1,
+  },
+  {
+    id: "t32",
+    name: "heyJacksonHouse2",
+    picture: heyjackson2,
+  },
+  {
+    id: "v48",
+    name: "heyJacksonHouse3",
+    picture: heyjackson3,
+  },
+  {
+    id: "wk9",
+    name: "heyJacksonHouse4",
+    picture: heyjackson4,
+  },
+  {
+    id: "y7z",
+    name: "heyJacksonHouse5",
+    picture: heyjackson5,
+  },
+];
