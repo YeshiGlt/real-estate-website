976b00e29a0f9dafe49a3ac20d08e14649ae84b3
YeshiGlt <39733077+YeshiGlt@users.noreply.github.com>
2023-03-08 09:27:11 +0000

    🚀 RELEASE: Organisation des datalist de la page

---
+++ src/datas/agentList.js
@@ -0,0 +1,27 @@
+import monikaPicture1 from "../assets/profil-pictures/monika-hasti-picture-1.png";
+import monikaPicture2 from "../assets/profil-pictures/monika-hasti-picture-2.png";
+import monikaBanner from "../assets/profil-pictures/monika-banner.png";
+import jateenPicture from "../assets/profil-pictures/jateen-ray.png";
+
+export const agentList = [
+  {
+    id: "f9d",
+    name: "Monika Hasti",
+    photo1: monikaPicture1,
+    photo2: monikaPicture2,
+    headingBanner: monikaBanner,
+    statutAgent: "Pro",
+    details:
+      "She was born in India in 1993, after continuing her education, Entered in Real Estate - 2015",
+    phoneNumber: "+0000000000",
+  },
+  {
+    id: "s3w",
+    name: "Jateen Ray",
+    photo: jateenPicture,
+    headingBanner: "",
+    statutAgent: "Expert",
+    details: "",
+    phoneNumber: "",
+  },
+];
